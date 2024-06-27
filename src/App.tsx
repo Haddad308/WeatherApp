6087a95d70d0356f5c93ac5da3d44b4babdf2f13
Haddad308 <melhaddad308@gmail.com>
2024-06-27 20:54:29 +0300

    All configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
+import { Avatar, Button } from "@nextui-org/react";
 
 function App() {
-
-  
   return (
-   <div>
-    <h1 className='bg-red-300 text-center' >
-      hello
-    </h1>
-   </div>
-  )
+    <div>
+      <h1 className="bg-red-300 text-center">hello</h1>
+      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+      <Button color="primary">Button</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
